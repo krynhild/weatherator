69c0cd716cf4397e2c22fd99c61f9834958156cc
Galina Kasatkina <Galina_Kasatkina@epam.com>
2020-08-16 13:41:38 +0200

    Prettified search input, added clear icon and action

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -3,14 +3,20 @@ import { makeStyles } from "@material-ui/core";
 import { useDispatch } from "react-redux";
 import { clear, load } from "../../store/data.actions";
 import TextField from "@material-ui/core/TextField";
+import ClearIcon from "@material-ui/icons/Clear";
+import SearchIcon from '@material-ui/icons/Search';
+import IconButton from "@material-ui/core/IconButton";
+import InputAdornment from "@material-ui/core/InputAdornment";
 
 const useStyles = makeStyles({
   root: {
     "margin": '50px',
+    "max-width": "500px",
+    width: '100%',
     transition: "all 0.2s ease 0s"
   },
-  input: {
-    padding: "10px 15px 10px 15px"
+  textField: {
+    width: '100%'
   }
 })
 
@@ -25,14 +31,32 @@ export function Search() {
     !value.trim() && dispatch(clear());
   }
 
+  const clearInput = () => {
+    setValue("");
+    dispatch(clear());
+  }
+
   return (
     <form className={classes.root} noValidate autoComplete="off" onSubmit={submit}>
       <TextField
+        autoFocus
+        className={classes.textField}
         variant={"outlined"}
-        inputProps={{
-          className: classes.input
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton onClick={clearInput}>
+                <ClearIcon />
+              </IconButton>
+            </InputAdornment>
+          )
         }}
-        placeholder={"Enter your city name"}
+        label={"City Name"}
         value={value}
         onChange={({ currentTarget: { value } }) => setValue(value)} />
     </form>
